99418d6331eb7df7e9add41748c3825681122a36
aldaniluk <allldaniluk@gmail.com>
2017-07-27 09:35:52 +0300

    Added xml comments

--- Logic/Queue.cs
+++ Logic/Queue.cs
@@ -6,6 +6,10 @@ using System.Text;
 
 namespace Logic
 {
+    /// <summary>
+    /// Generic collection queue.
+    /// </summary>
+    /// <typeparam name="T">Type for substitution.</typeparam>
     public class Queue<T> : IEnumerable<T>, IEquatable<Queue<T>> where T : IEquatable<T> 
     {
         #region fields
@@ -17,16 +21,30 @@ namespace Logic
         #endregion
 
         #region properties
+        /// <summary>
+        /// Quantity of elements.
+        /// </summary>
         public int Count => count;
+
+        /// <summary>
+        /// Returns true, if queue is empty, and false otherwise.
+        /// </summary>
         public bool IsEmpty => count == 0;
         #endregion
 
         #region Ctors
+        /// <summary>
+        /// Ctor without parameters.
+        /// </summary>
         public Queue()
         {
             array = new T[capacity];
         }
 
+        /// <summary>
+        /// Ctor with parameter.
+        /// </summary>
+        /// <param name="capacity">Initial size of queue.</param>
         public Queue(int capacity)
         {
             if (capacity <= 0) throw new ArgumentException($"{nameof(capacity)} is unsuitable.");
@@ -35,6 +53,10 @@ namespace Logic
             array = new T[capacity];
         }
 
+        /// <summary>
+        /// Ctor with parameter.
+        /// </summary>
+        /// <param name="collection">Collection to copy elements into queue.</param>
         public Queue(IEnumerable<T> collection)
         {
             if (ReferenceEquals(collection, null)) throw new ArgumentNullException($"{nameof(collection)} is null.");
@@ -48,6 +70,10 @@ namespace Logic
         #endregion
 
         #region public methods
+        /// <summary>
+        /// Add an element into queue.
+        /// </summary>
+        /// <param name="element">Element ro insert.</param>
         public void Enqueue(T element)
         {
             if (ReferenceEquals(element, null)) throw new ArgumentNullException($"{nameof(element)} is null.");
@@ -58,6 +84,9 @@ namespace Logic
             count++;
         }
 
+        /// <summary>
+        /// Removes an element from queue.
+        /// </summary>
         public void Dequeue()
         {
             if (IsEmpty) throw new ArgumentException("Queue is empty.");
@@ -69,6 +98,10 @@ namespace Logic
             array[--count] = default(T);
         }
 
+        /// <summary>
+        /// Removes some elements from queue.
+        /// </summary>
+        /// <param name="quantity">Quantity of elements to remove.</param>
         public void Dequeue(int quantity)
         {
             if (quantity < 0 || quantity > array.Length) throw new ArgumentNullException($"{nameof(quantity)} is unsuitable.");
@@ -80,6 +113,10 @@ namespace Logic
             }
         }
 
+        /// <summary>
+        /// Returns first element in the queue.
+        /// </summary>
+        /// <returns>First element in the queue.</returns>
         public T Peek()
         {
             if (IsEmpty) throw new ArgumentException("Queue is empty.");
@@ -87,6 +124,11 @@ namespace Logic
             return array[0];
         }
 
+        /// <summary>
+        /// Checks the presence of the element in the queue.
+        /// </summary>
+        /// <param name="element">Element to check.</param>
+        /// <returns>True, if element presents, and false otherwise.</returns>
         public bool Contains(T element)
         {
             if (IsEmpty) throw new ArgumentException("Queue is empty.");
@@ -100,6 +142,9 @@ namespace Logic
             return false;
         }
 
+        /// <summary>
+        /// Clears the queue.
+        /// </summary>
         public void Clear()
         {
             if (IsEmpty) throw new ArgumentException("Queue is empty.");
@@ -109,6 +154,10 @@ namespace Logic
             array = new T[capacity];
         }
 
+        /// <summary>
+        /// Returns string representation of queue.
+        /// </summary>
+        /// <returns>String representation of queue.</returns>
         public override string ToString()
         {
             if (IsEmpty) return "Queue is empty.";
@@ -121,6 +170,11 @@ namespace Logic
             return str.ToString();
         }
 
+        /// <summary>
+        /// Compares two queues for equality.
+        /// </summary>
+        /// <param name="other">Second queue to compare.</param>
+        /// <returns>True, if queues are equal, and false otherwise.</returns>
         public bool Equals(Queue<T> other)
         {
             if (ReferenceEquals(other, null)) throw new ArgumentNullException($"{nameof(other)} is null.");
@@ -135,7 +189,16 @@ namespace Logic
             return true;
         }
 
+        /// <summary>
+        /// Enumerator for queue.
+        /// </summary>
+        /// <returns>Object IEnumerator<T> to iterate.</returns>
         public IEnumerator<T> GetEnumerator() => new IteratorQueue(this);
+
+        /// <summary>
+        /// Enumerator for queue.
+        /// </summary>
+        /// <returns>Object IEnumerator to iterate.</returns>
         IEnumerator IEnumerable.GetEnumerator() => new IteratorQueue(this);
 
         #region struct IteratorQueue
@@ -153,7 +216,7 @@ namespace Logic
                 }
             }
 
-            object IEnumerator.Current => queue.array[index];
+            object IEnumerator.Current => Current;
 
             public IteratorQueue(Queue<T> queue)
             {
